5687a34685c00ba37eafbcc00b2936f098e7b74a
dimilidi <dimitrovalidia@yahoo.com>
2024-09-03 18:17:59 +0200

    BestTimeToBuyAndSellStocks

--- src/P4_BestTimeToBuyAndSellStocks/BestTimeToBuyAndSellStocks.java
+++ src/P4_BestTimeToBuyAndSellStocks/BestTimeToBuyAndSellStocks.java
@@ -3,9 +3,11 @@ package P4_BestTimeToBuyAndSellStocks;
 public class BestTimeToBuyAndSellStocks {
 
     public static void main(String[] args) {
-        int[] prices = {7,6,4,3,1};
+        int[] prices1 = {7,6,4,3,1};
+        int[] prices2 = {7,1,5,3,6,4};
 
-        System.out.println(maxProfit(prices));
+        System.out.println(maxProfit(prices1));
+        System.out.println(maxProfit(prices2));
 
     }
 
